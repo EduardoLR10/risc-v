32034b9a944174885da3e99537ac6fae69cea112
brenoafb <breno.afb@gmail.com>
2021-03-31 10:52:07 -0300

    Add instruction memory testbench module

---
+++ processor/src/instruction_memory/instruction_memory_tb.cpp
@@ -0,0 +1,15 @@
+#include "instruction_memory/instruction_memory_tb.h"
+
+void InstructionMemoryTB::activate()
+{
+  // wait for memory contents to be read
+  clock = 0;
+  wait(10, SC_NS); 
+  address = 0x00000000;
+  clock = 1;
+  wait(1, SC_NS);
+  clock = 0;
+  std::cout << "address = " << std::setw(W) << std::hex << address.read();
+  std::cout << "output = " << out.read() << std::endl
+            << std::endl;
+}
