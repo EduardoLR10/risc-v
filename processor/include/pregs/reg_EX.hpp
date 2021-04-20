cab5009827c6618a88218680c14ccead0a48e22e
RicardoJacobi <ricardojacobi@gmail.com>
2021-04-20 18:47:37 -0300

    Update reg_EX.hpp

--- processor/include/pregs/reg_EX.hpp
+++ processor/include/pregs/reg_EX.hpp
@@ -6,15 +6,23 @@
 
 // ************************** ID/EX
 SC_MODULE (reg_EX){
-    
+    // control
+    sc_in< sc_uint<4> > id_ex_ctrl;
+    sc_in< sc_uint<3> > id_mem_ctrl;
+    sc_in< sc_uint<2> > id_wb_ctrl;
+    sc_out< bool > alu_src_A, alu_src_B;
+    sc_out< sc_uint<2> > alu_op;
+    sc_out< sc_uint<3> > mem_ctrl;
+    sc_out< sc_uint<2> > wb_ctrl;
+
     // data
     sc_in< sc_uint<5> > id_rs1, id_rs2, id_rd;
-    sc_in< sc_uint<SIZE> > id_imm, id_regA, id_regB;
+    sc_in< sc_uint<WSIZE> > id_imm, id_regA, id_regB, id_pc;
     sc_in<bool> clk, wren, rst;
     sc_in< sc_uint<7> > id_funct7;
     sc_in< sc_uint<3> > id_funct3;
     sc_out< sc_uint<5> > ex_rs1, ex_rs2, ex_rd;
-    sc_out< sc_uint<SIZE> > ex_imm, ex_regA, ex_regB;
+    sc_out< sc_uint<SIZE> > ex_imm, ex_regA, ex_regB, ex_pc;
     sc_out< sc_uint<7> > ex_funct7;
     sc_out< sc_uint<3> > ex_funct3;
 
@@ -26,7 +34,7 @@ SC_MODULE (reg_EX){
     }
 
 private:
-    sc_uint<SIZE> imm_buf, regA_buf, regB_buf;
+    sc_uint<SIZE> imm_buf, regA_buf, regB_buf, pc_buf;
     sc_uint<5> rs1_buf, rs2_buf, rd_buf;
     sc_uint<7> funct7_buf;
     sc_uint<3> funct3_buf;
