59f01c918d96142c497cd3ac5dacb78cb5f68e55
RicardoJacobi <ricardojacobi@gmail.com>
2021-04-20 12:23:37 -0300

    Update signals.hpp

--- processor/include/signals/signals.hpp
+++ processor/include/signals/signals.hpp
@@ -20,14 +20,21 @@ sc_signal< sc_uint<SIZE> >
         id_imm_ws,              // immediate generated at IF/ID
         breg_ra,                // first breg output - a
         breg_rb,                // second breg output - b
+
+sc_signal< sc_uint<7> >
+        id_ins_code;            // opcode of RV instruction
+
+sc_signal< sc_uint<WSIZE> >
         // Execute stage
         ex_ra,                  // breg a output at execution stage
         ex_rb,                  // breg b output at execution stage
+        ex_imm,                 // immediate at execution stage
         mux_alu_a,              // ALU input A
         mux_alu_b,              // ALU input B
         alu_out,                // ALU output
         // Memory stage
         mem_alu_out,            // ALU output at memory stage
+        mem_mux_alu_a,          // ALU input B at mem stage
         mem_mux_alu_b,          // ALU input B at mem stage
         dm_out,                 // Data memory output
         // Writeback stage
@@ -37,8 +44,6 @@ sc_signal< sc_uint<SIZE> >
 
 sc_signal< sc_uint<5> >
         // Decode stage
-        id_ra,                  // breg ra instruction field
-        id_rb,                  // breg rb instruction field
         id_rs1,                 // rs1 instruction field in IF/ID
         id_rs2,                 // rs2 instruction field in IF/ID
         id_rd,                  // rd instruction field in IF/ID
@@ -51,7 +56,54 @@ sc_signal< sc_uint<5> >
         // Memory stage
         wb_rd;                  // rd field in memory stage
 
+
+        // Control Signals
+
+        // Fetch -----------------------------------------------------------
+sc_signal< bool > sel_mux_pc;       // select pc input
+sc_signal<bool>
+        wren_reg_ID,                // enable write on reg IF/ID - hazard_rv
+        rst_reg_ID,                 // reset reg_ID
+        wren_pc;                    // enable write PC
+
+        // Decode ----------------------------------------------------------
 sc_signal< sc_uint<2> >
-        sel_mux_pc;             // select pc input
+        id_wb_cntr,
+        b_opcode;                   // 0: beq  1: bne  2: blt  3: bge
+sc_signal< sc_uint<5> >
+        id_ex_cntr;
+sc_signal< sc_uint<3> >
+        id_mem_cntr;
+sc_signal<bool>
+        b_cond,                     // condition for branch (output dbranch)
+        is_jal,                     // identifies jal and jalr
+        is_jalr,                    //
+        is_branch,                  // identifies branches
+        id_flush,                   // insert bubble at decode stage
+        wren_breg;                  // enable write on breg
+
+        // Execute ---------------------------------------------------------
+sc_signal< bool >
+        sel_alu_B,                  // select ALU B input
+                                    // 0: ex_rb; 1: ex_imm
+        ex_flush;                   // insert bubble at execute stage
+
+sc_signal< sc_uint<2> >
+        sel_alu_A;                  // select ALU A input
+                                    // 0: zero; 1: ex_pc; 2: ex_ra
+
+sc_signal< bool >
+        alu_op;                     // instruction type:
+                                    // 0: memory access
+                                    // 1: arith-logic operations
+        // Memory ----------------------------------------------------------
+sc_signal< sc_uint<2> >
+        mem_data_size;              // 0: byte, 1: half, 2: word
+sc_signal< bool >
+        mem_rw;                     // 0: read, 1: write
+
+        // WriteBack -------------------------------------------------------
+sc_signal< bool >
+        wb_sel_mux;                  // 0: memory, 1: ALU
         
 #endif //__SIGNALS_H__
