76d277d26da9831e6c2df7102803c6a8b8df098d
RicardoJacobi <ricardojacobi@gmail.com>
2021-04-20 19:06:22 -0300

    Update reg_MEM.hpp

--- processor/include/pregs/reg_MEM.hpp
+++ processor/include/pregs/reg_MEM.hpp
@@ -6,11 +6,21 @@
 
 // ************************** EX/MEM
 SC_MODULE (reg_MEM){
+// Control
+    sc_in< sc_uint<3> > ex_mem_ctrl;
+    sc_in< sc_uint<2> > ex_wb_ctrl;
+// Mem control
+    sc_out< bool > rd_wr;  // 0 - read; 1 - write
+    sc_out< sc_uint<2> > d_size; // 00 - byte; 01 - half; 10 - word
+// WB control
+    sc_out< sc_uint<2> > mem_wb_ctrl;
+
+// Data
     sc_in< sc_uint<5> > ex_rd;
-    sc_in< sc_uint<SIZE> > ex_alu_out, ex_mux_alu_b;
+    sc_in< sc_uint<WSIZE> > ex_alu_out, ex_mux_alu_b;
     sc_in<bool> clk, wren, rst;
     sc_out< sc_uint<5> > mem_rd;
-    sc_out< sc_uint<SIZE> > mem_alu_out, mem_mux_alu_b;
+    sc_out< sc_uint<WSIZE> > mem_alu_out, mem_mux_alu_b;
 
     void load();
 
