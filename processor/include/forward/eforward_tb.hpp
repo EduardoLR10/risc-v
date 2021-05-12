5c8ce999575c3a60dd16c7dbd262df23172edf1b
pvvm <pv.mizuno@hotmail.com>
2021-05-12 18:45:30 -0300

    Changed to eforward_tb.hpp

--- processor/include/forward/eforward_tb.hpp
+++ processor/include/forward/eforward_tb.hpp
@@ -1,16 +1,16 @@
-#ifndef __FORWARD_TB__
-#define __FORWARD_TB__
+#ifndef __EFORWARD_TB__
+#define __EFORWARD_TB__
 
 #include <systemc.h>
 
-SC_MODULE(forward_tb) {
+SC_MODULE(eforward_tb) {
 	sc_out< sc_uint<5> > ID_EX_rs1, ID_EX_rs2, EX_MEM_rd, MEM_WB_rd;
 	sc_out< bool > EX_MEM_write, MEM_WB_write;
 	sc_in< sc_uint<2> > forwardA, forwardB;
 
 	void aciona();
 
-	SC_CTOR(forward_tb) {
+	SC_CTOR(eforward_tb) {
 		SC_THREAD(aciona);
 	}
 };
