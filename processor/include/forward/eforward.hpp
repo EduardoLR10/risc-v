0978482ff5310b8d721c3f2e4a709893481ba0e4
pvvm <pv.mizuno@hotmail.com>
2021-05-12 18:44:55 -0300

    Changed eforward.hpp

--- processor/include/forward/eforward.hpp
+++ processor/include/forward/eforward.hpp
@@ -1,16 +1,16 @@
-#ifndef __FORWARD__
-#define __FORWARD__
+#ifndef __EFORWARD__
+#define __EFORWARD__
 
 #include <systemc.h>
 
-SC_MODULE(forward) {
+SC_MODULE(eforward) {
 	sc_in< sc_uint<5> > ID_EX_rs1, ID_EX_rs2, EX_MEM_rd, MEM_WB_rd;
 	sc_in< bool > EX_MEM_write, MEM_WB_write;
 	sc_out< sc_uint<2> > forwardA, forwardB;
 
 	void proc(void);
 
-	SC_CTOR(forward) {
+	SC_CTOR(eforward) {
 		SC_METHOD(proc);
 		sensitive << ID_EX_rs1 << ID_EX_rs2 << EX_MEM_rd << MEM_WB_rd << EX_MEM_write << MEM_WB_write;
 	}
