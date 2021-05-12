50656a2ea65af22fff5c543601dec9718e8e4413
Eduardo Lemos Rocha <dudulr10@gmail.com>
2021-05-12 18:50:57 -0300

    Update wforward_tb.hpp

--- processor/include/wforward/wforward_tb.hpp
+++ processor/include/wforward/wforward_tb.hpp
@@ -1,18 +1,18 @@
-#ifndef __WFORWARD_TB__
-#define __WFORWARD_TB__
+#ifndef __DFORWARD_TB__
+#define __DFORWARD_TB__
 
 #include <systemc.h>
 #include "top/macros.hpp"
 #include <string>
 #include <iomanip>
 
-SC_MODULE (wforward_tb) {
+SC_MODULE (dforward_tb) {
     sc_out< sc_uint<5> > rs1, rs2, rd;
     sc_in<bool> rAOut, rBOut;
 
     void aciona();
 
-    SC_CTOR(wforward_tb) {
+    SC_CTOR(dforward_tb) {
         SC_THREAD(aciona);
     }
 };
