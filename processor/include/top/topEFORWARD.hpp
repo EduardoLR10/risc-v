1a46981fe7e170da311ced6d588c3b22773fc080
pvvm <pv.mizuno@hotmail.com>
2021-05-12 19:02:15 -0300

    Changed to topEFORWARD.hpp

--- processor/include/top/topEFORWARD.hpp
+++ processor/include/top/topEFORWARD.hpp
@@ -1,16 +1,16 @@
 #include <systemc.h>
-#include "forward/forward.hpp"
-#include "forward/forward_tb.hpp"
+#include "eforward/eforward.hpp"
+#include "eforward/eforward_tb.hpp"
 
-SC_MODULE ( topFORWARD ) {
-	forward FORWARD;
-	forward_tb FORWARD_tb;
+SC_MODULE ( topEFORWARD ) {
+	eforward FORWARD;
+	eforward_tb FORWARD_tb;
 
 	sc_signal< sc_uint<5> > ID_EX_rs1, ID_EX_rs2, EX_MEM_rd, MEM_WB_rd;
 	sc_signal< bool >	EX_MEM_write, MEM_WB_write;
 	sc_signal< sc_uint<2> > forwardA, forwardB;
 
-	SC_CTOR( topFORWARD ) : FORWARD("FORWARD"), FORWARD_tb("FORWARD_tb") {
+	SC_CTOR( topEFORWARD ) : FORWARD("FORWARD"), FORWARD_tb("FORWARD_tb") {
 		FORWARD.ID_EX_rs1(ID_EX_rs1);			FORWARD_tb.ID_EX_rs1(ID_EX_rs1);
 		FORWARD.ID_EX_rs2(ID_EX_rs2);			FORWARD_tb.ID_EX_rs2(ID_EX_rs2);
 		FORWARD.EX_MEM_rd(EX_MEM_rd);			FORWARD_tb.EX_MEM_rd(EX_MEM_rd);
