4032a7e9bec80a2319b60c604826cc714c40f697
Eduardo Lemos Rocha <dudulr10@gmail.com>
2021-05-12 18:50:33 -0300

    Update wforward.hpp

--- processor/include/wforward/wforward.hpp
+++ processor/include/wforward/wforward.hpp
@@ -1,16 +1,16 @@
-#ifndef __WFORWARD__
-#define __WFORWARD__
+#ifndef __DFORWARD__
+#define __DFORWARD__
 
 #include <systemc.h>
 #include "top/macros.hpp"
 
-SC_MODULE (wforward) {
+SC_MODULE (dforward) {
     sc_in< sc_uint<5> > rs1, rs2, rd;
     sc_out<bool> rAOut, rBOut;
 
     void proc(void);
 
-    SC_CTOR(wforward) {
+    SC_CTOR(dforward) {
         SC_METHOD(proc);
         sensitive << rs1 << rs2 << rd;
     }
